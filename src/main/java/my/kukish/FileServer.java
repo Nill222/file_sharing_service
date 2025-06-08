148979d43dc1023109b57ac11b4530a74d1cd731
churkaa <semenovegor546@gmail.com>
2025-06-08 23:00:48 +0300

    Server

---
+++ src/main/java/my/kukish/FileServer.java
@@ -0,0 +1,30 @@
+package my.kukish;
+
+import com.sun.net.httpserver.HttpServer;
+import my.kukish.handler.DownloadHandler;
+import my.kukish.handler.UploadHandler;
+import my.kukish.service.FileStorageService;
+
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.concurrent.Executors;
+
+public class FileServer {
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+        Files.createDirectories(Paths.get("uploads"));
+
+        FileStorageService storageService = new FileStorageService("uploads");
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.createContext("/upload", new UploadHandler(storageService));
+        server.createContext("/download", new DownloadHandler(storageService));
+
+        server.setExecutor(Executors.newCachedThreadPool());
+        server.start();
+        System.out.println("Сервер запущен на http://localhost:" + port);
+    }
+}
